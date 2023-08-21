5a0734e07e709bcc211e43fece9911c1b83fd7e5
Glitch (thoughtful-vagabond-fibre) <none>
2023-08-21 11:23:48 +0000

    💄🛣 Checkpoint ./index.js:52870225/9

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
-const middleware = jsonServer.defaults({readOnly: true})
+const middleware = jsonServer.defaults({readOnly: false})
 
 const { PORT = 3000 } = process.env
 
